9899b0ea060991cf9740dc5a9c1810c93a110342
Luke <lukuan1992@sina.com>
2017-07-20 23:10:51 +0800

    extend activity

--- app/src/main/java/com/luke/luke/helloworld/NewsActivity.java
+++ app/src/main/java/com/luke/luke/helloworld/NewsActivity.java
@@ -1,8 +1,12 @@
 package com.luke.luke.helloworld;
 
+import android.app.Activity;
+
 /**
  * Created by luke on 2017/7/20.
  */
 
-public class NewsActivity {
+public class NewsActivity extends Activity{
+
+
 }
